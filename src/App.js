081ae438630f5b176cbc0abd9730547f6c154f81
den8795 <den8795@gmail.com>
2018-10-28 01:05:35 +0300

    commit project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
+import {getData} from './actions/actions'
 
-import MainContainer from './container/index'
+import MainComponent from './components/mainComponent'
+
+
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import connect from "react-redux/es/connect/connect";
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            prevUser: 5
+        }
+    }
+
+    componentDidMount() {
+        this.props.getUser();
+    }
+
   render() {
+      const {prevUser} = this.state;
+      const {dataUsers, loading, error} = this.props;
+
+      let pages = [];
+
+      for (let i = 1; i <= Math.ceil(dataUsers.length / prevUser ); i++){
+          pages.push(i)
+      }
+
+
+      if (loading){
+          return <div>Loading...</div>
+      }
+
+      if(error){
+          return <div>Error! {error.message}</div>
+      }
+
+
     return (
       <div className="App">
-          <MainContainer/>
+          <Router>
+              <div>
+                  <h2>Users</h2>
+                  {
+                    pages.map(id =>(
+                        <button key={id}>
+                            <Link to={'/' + id}>{id}</Link>
+                        </button>
+                    ))
+                  }
+                  <Route path='/:id' render={(props) => <MainComponent {...props} dataUsers = {dataUsers}/>} />
+              </div>
+          </Router>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+    dataUsers: state.dataUsers.items,
+    loading: state.dataUsers.loading,
+    error: state.dataUsers.error
+});
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getUser: () => dispatch(getData())
+    }
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+
--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,71 +1,42 @@
 import React, {Component} from 'react';
 
 
-
-
 class MainComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentPage: 1,
             prevPage: 5
         };
-        this.handleClick = this.handleClick.bind(this)
-    }
-
-    handleClick(event){
-        this.setState({
-            currentPage: event.target.id
-        })
     }
 
 
-
-    render() {
+    render(){
         const {dataUsers} = this.props;
-        const {currentPage, prevPage} = this.state;
+        const {prevPage} = this.state;
+        const {id} = this.props.match.params;
+        console.log(this.props);
 
-        const indexOfLastUser = currentPage * prevPage;
+
+        const indexOfLastUser = id * prevPage;
         const indexOfFirstUser = indexOfLastUser - prevPage;
         const currentUsers = dataUsers.slice(indexOfFirstUser, indexOfLastUser);
 
 
-        const renderUsers  = currentUsers.map(item=>(
+        const renderUsers = currentUsers.map(item=>(
            <div key={item.id}>
-               <span style={{fontSize:'20px'}}>{item.name} </span>
-               <span style={{fontSize:'20px'}}>{item.surname}</span>
+               <span style={{fontSize:'25px'}}>{item.name} </span>
+               <span style={{fontSize:'25px'}}>{item.surname}</span>
                <p>{item.desc}</p>
            </div>
         ));
 
-        let pagesCount = [];
-
-        for (let i = 1; i<= Math.ceil(dataUsers.length / prevPage); i++){
-            pagesCount.push(i);
-        }
-
-        const renderPageNumbers = pagesCount.map(number =>{
-            return(
-                <button
-                    key={number}
-                    id={number}
-                    onClick={this.handleClick}
-                >
-                    {number}
-                </button>
-            )
-        });
-
-        return (
+        return(
             <div>
                 <div>
                     {renderUsers}
                 </div>
-                <div>
-                    {renderPageNumbers}
-                </div>
             </div>
-        );
+        )
     }
 }
 
